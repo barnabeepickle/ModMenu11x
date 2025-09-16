cb1ae21fb17158e58dabbbc5a115d7adca29441d
barnabeepickle <linusrk30@gmail.com>
2025-09-16 11:45:32 -0400

    switched text over to component

--- src/main/java/com/github/barnabeepickle/modmenu/mixin/MixinTitleScreen.java
+++ src/main/java/com/github/barnabeepickle/modmenu/mixin/MixinTitleScreen.java
@@ -6,7 +6,7 @@ import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.screen.TitleScreen;
 import net.minecraft.client.gui.widget.AbstractButtonWidget;
 import net.minecraft.client.resource.language.I18n;
-import net.minecraft.text.Text;
+import net.minecraft.network.chat.Component;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -16,7 +16,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(TitleScreen.class)
 public class MixinTitleScreen extends Screen {
 
-	public MixinTitleScreen(Text title) {
+	public MixinTitleScreen(Component title) {
 		super(title);
 	}
 
